870167b686b9309cccc6a7df9eb0b6e040bc5946
ahmed37 <ahmed.hlissa@gmail.com>
2018-02-02 22:53:59 +0000

    source conttrole

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ProductListComponent } from '../products/product-list.component';
+import { ConvertToSpacePipe } from './shared/conver-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'
+
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacePipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path : 'products', component: ProductListComponent},
+      { path : 'products/id' , component :ProductDetailComponent},
+      { path : 'welcome', component: WelcomeComponent},
+      { path : 'defaul', redirectTo: 'welcome',pathMatch: 'full'},
+      { path : '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
